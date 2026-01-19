0d7318ebe6e56004aa66804fb280b9600b340118
sumedhjahagirdar <sumedhj2007@gmail.com>
2026-01-20 00:31:42 +0530

    Security update: move password to environment variables

--- db.php
+++ db.php
@@ -3,19 +3,17 @@
 $host = 'mysql-e537ae1-sumedhj2007-169b.j.aivencloud.com';
 $port = '26628';
 $user = 'avnadmin';
-$pass = 'AVNS_KbTx9BWnMNJmjm1lBkk'; 
+
+// SECURITY: Fetch password from Vercel's Environment Variables
+$pass = getenv('DB_PASSWORD'); 
+
 $db   = 'orphanage_db';
 
-// Aiven requires SSL for security.
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-
-// Establish the connection
 $link = mysqli_real_connect($conn, $host, $user, $pass, $db, $port);
 
 if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-// We will use $conn in all other files to perform queries.
 ?>
